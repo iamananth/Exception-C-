e912d1384ade9f0a9c353427fc9819a1998b96b1
iamananth <ananthrshenoy@gmail.com>
2022-07-08 10:15:37 +0530

    Arithmetic exception

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -4,10 +4,34 @@ namespace ConsoleApp6
 {
     class Program
     {
+        static void checkAge(int age)
+        {
+            if (age < 18)
+            {
+                throw new ArithmeticException("Access Denied");
+            }
+            else
+            {
+                Console.WriteLine("Access Granted");
+            }
+        }
         static void Main(String[] args)
         {
-            int[] mynumbers = { 1, 2, 3, 4, 5, 6, };
-            Console.WriteLine(mynumbers[10]);
+            /* try
+             {
+                 int[] mynumbers = { 1, 2, 3, 4, 5, 6 };
+                 Console.WriteLine(mynumbers[10]);
+             }
+             catch(Exception e)
+             {
+                 Console.WriteLine("Sorryyyyy");
+             }
+             finally
+             {
+                 Console.WriteLine("Finally");
+             }
+            */
+            checkAge(15);
         }
     }
 }
