0c610935b9bd87f87851d72d0e9912584fca9d9f
iamananth <ananthrshenoy@gmail.com>
2022-07-08 09:31:03 +0530

    commit

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int[] mynumbers = { 1, 2, 3, 4, 5, 6, };
+            Console.WriteLine(mynumbers[10]);
+        }
+    }
+}
